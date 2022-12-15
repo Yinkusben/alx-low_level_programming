58ab29adf0d2f071280fb6c4d2f8449696b65590
Yinkusben <Yinkusben4sure@gmail.com>
2022-12-15 15:49:31 -0800

    added files

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _is upper checks for uppercase character
+ * @c - the characyer to be checked
+ * Returns 1 if c is upoercase, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	return(isupper(c));
+}
