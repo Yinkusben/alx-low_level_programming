e3e2c266e64cc9f4682a0d10b643b3aee6409afb
Yinkusben <yinkusben4sure@gmail.com>
2023-01-16 18:44:44 +0000

    made changes to dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,7 +9,6 @@
  * @owner: dog owner
  *
 */
-
 struct dog
 {
 	char *name;
@@ -17,9 +16,14 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t -Typedef for struct dog
+*/
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
-#endif
+#endif /* struct dog */
