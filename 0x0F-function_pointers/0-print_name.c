ce4e9962b384473c672750f0b64d62b6a221e286
Yinkusben <yinkusben4sure@gmail.com>
2023-01-18 22:53:31 +0000

    added files

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - a funtion that prints name
+ * @name: the name to print
+ * @f: the function pointer to the meethod to use in
+ * printing the name
+ *
+ * Return: Nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
